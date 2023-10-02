224f3d07de30409df3b4eb59876eafade0577d09
Zuzana Borutova <z.kasikova@gmail.com>
2023-10-02 17:46:55 +0200

    první commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import CakeIcon from '@mui/icons-material/Cake';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={CakeIcon} alt="icon" />
+        <Stack direction="row" spacing={2}>
+          <Button color="secondary">Secondary</Button>
+          <Button variant="contained" color="success">
+            Success
+          </Button>
+          <Button variant="outlined" color="error">
+            Error
+          </Button>
+        </Stack>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
