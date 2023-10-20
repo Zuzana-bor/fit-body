50b465484b5a385656f38e1c1cfb8a0ae90d7ef8
Zuzana-bor <z.kasikova@gmail.com>
2023-10-20 14:32:41 +0200

    odpovědi uloženy do stavu

--- src/pages/questions/SelectQuestion.jsx
+++ src/pages/questions/SelectQuestion.jsx
@@ -1,26 +1,14 @@
 import * as React from 'react';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import { includes } from 'lodash';
-import { selectAsk } from '../../questions';
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
-
 import Select from '@mui/material/Select';
 
 const SelectQuestion = ({
   ask: [ask, answer1, answer2, answer3, answer4, answer5, naswer6, answer7],
+  onAnswer,
 }) => {
-  const [age, setAge] = React.useState('');
-
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  };
-
   return (
     <>
       <Box sx={{ minWidth: 120 }}>
@@ -29,17 +17,19 @@ const SelectQuestion = ({
           <Select
             labelId="demo-simple-select-label"
             id="demo-simple-select"
-            value={age}
-            label="Age"
-            onChange={handleChange}
+            name={ask.ask}
+            label="ask"
+            onChange={(event) => {
+              onAnswer(event);
+            }}
           >
             <MenuItem value={ask.answer1}>{ask.answer1}</MenuItem>
             <MenuItem value={ask.answer2}>{ask.answer2}</MenuItem>
-            <MenuItem value={ask.answer3}>{answer3}</MenuItem>
+            <MenuItem value={ask.answer3}>{ask.answer3}</MenuItem>
             <MenuItem value={ask.answer4}>{ask.answer4}</MenuItem>
             <MenuItem value={ask.answer5}>{ask.answer5}</MenuItem>
             <MenuItem value={ask.answer6}>{ask.answer6}</MenuItem>
-            <MenuItem value={ask.answer8}>{ask.answer7}</MenuItem>
+            <MenuItem value={ask.answer7}>{ask.answer7}</MenuItem>
           </Select>
         </FormControl>
       </Box>
