69a2f9d3945158096c783d0a48bd86786087714f
Zuzana-bor <z.kasikova@gmail.com>
2023-10-21 07:41:43 +0200

    přidány ikony nad dotazníkem

--- src/pages/questions/Questions.jsx
+++ src/pages/questions/Questions.jsx
@@ -3,11 +3,14 @@ import TextQuestion from './TextQuestion';
 import { asksText } from '../../questions';
 import { selectAsk } from '../../questions';
 import SelectQuestion from './SelectQuestion';
-
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import { Typography } from '@mui/material';
+import FitnessCenterTwoToneIcon from '@mui/icons-material/FitnessCenterTwoTone';
+import EggAltTwoToneIcon from '@mui/icons-material/EggAltTwoTone';
+import MoodTwoToneIcon from '@mui/icons-material/MoodTwoTone';
 
 const Questions = () => {
   const [values, setValues] = React.useState({});
@@ -16,10 +19,25 @@ const Questions = () => {
     setValues((prev) => ({ ...prev, [event.target.name]: event.target.value }));
   };
 
-  console.log(values);
-
   return (
     <>
+      <Typography variant="h4" m={3}>
+        Zjisti:
+      </Typography>
+      <Stack direction="row" justifyContent="space-around">
+        <Stack alignItems="center">
+          <FitnessCenterTwoToneIcon fontSize="large" color="primary" />
+          <Typography variant="h6">Jak si tréninkgy nakombinovat.</Typography>
+        </Stack>
+        <Stack alignItems="center">
+          <EggAltTwoToneIcon fontSize="large" color="primary" />
+          <Typography variant="h6">Jak se stravovat</Typography>
+        </Stack>
+        <Stack alignItems="center">
+          <MoodTwoToneIcon fontSize="large" color="primary" />
+          <Typography variant="h6">Jak být v pohodě a šťastná</Typography>
+        </Stack>
+      </Stack>
       <Box m={10}>
         <Paper elevation={3} sx={{ p: 5 }}>
           {Object.entries(asksText).map(([key, value]) => (
