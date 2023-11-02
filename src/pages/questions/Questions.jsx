a12d8ff41d42dcd8c3e68217fd6d0b94a650c2b6
Zuzana-bor <z.kasikova@gmail.com>
2023-11-02 16:26:23 +0100

    otázky pod sebou, přidány radiobuttony, key a value anglicky

--- src/pages/questions/Questions.jsx
+++ src/pages/questions/Questions.jsx
@@ -1,8 +1,4 @@
 import * as React from 'react';
-import TextQuestion from './TextQuestion';
-import { asksText } from '../../questions';
-import { selectAsk } from '../../questions';
-import SelectQuestion from './SelectQuestion';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
@@ -12,16 +8,33 @@ import FitnessCenterTwoToneIcon from '@mui/icons-material/FitnessCenterTwoTone';
 import EggAltTwoToneIcon from '@mui/icons-material/EggAltTwoTone';
 import MoodTwoToneIcon from '@mui/icons-material/MoodTwoTone';
 import Result from './Result';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
 
 const Questions = () => {
   const [values, setValues] = React.useState({});
+  const [isSubmitted, setIsSubmitted] = React.useState(false);
 
-  const handleAnswer = (event) => {
+  const handleChange = (event) => {
     setValues((prev) => ({ ...prev, [event.target.name]: event.target.value }));
   };
 
-  const handleCLick = () => {};
+  const handleSubmit = () => {
+    setIsSubmitted(true);
+  };
 
+  const isFilled = Object.values(values).length === 8;
+
+  console.log(values);
   return (
     <>
       <Typography variant="h4" m={3}>
@@ -41,30 +54,215 @@ const Questions = () => {
           <Typography variant="h6">Jak být v pohodě a šťastná</Typography>
         </Stack>
       </Stack>
-      <Box m={10}>
-        <Paper elevation={3} sx={{ p: 5 }}>
-          {Object.entries(asksText).map(([key, value]) => (
-            <TextQuestion ask={[value]} onAnswer={handleAnswer} />
-          ))}
-          {selectAsk.map((ask) => (
-            <SelectQuestion ask={[ask]} onAnswer={handleAnswer} />
-          ))}
-        </Paper>
-      </Box>
+      {!isSubmitted ? (
+        <>
+          <Box m={10}>
+            <Paper elevation={3} sx={{ p: 5 }}>
+              <Stack>
+                <TextField
+                  inputProps={{
+                    inputMode: 'numeric',
+                    pattern: '[0-9]*',
+                    startAdornment: (
+                      <InputAdornment position="start">kg</InputAdornment>
+                    ),
+                  }}
+                  id="outlined-basic"
+                  type="number"
+                  label="Váha"
+                  name="weight"
+                  variant="outlined"
+                  onChange={handleChange}
+                />
+                <TextField
+                  inputProps={{
+                    inputMode: 'numeric',
+                    pattern: '[0-9]*',
+                    startAdornment: (
+                      <InputAdornment position="start">cm</InputAdornment>
+                    ),
+                  }}
+                  id="outlined-basic"
+                  type="number"
+                  label="Výška"
+                  name="height"
+                  variant="outlined"
+                  onChange={handleChange}
+                />
+                <TextField
+                  inputProps={{
+                    inputMode: 'numeric',
+                    pattern: '[0-9]*',
+                    startAdornment: (
+                      <InputAdornment position="start">cm</InputAdornment>
+                    ),
+                  }}
+                  id="outlined-basic"
+                  type="number"
+                  label="Věk"
+                  name="age"
+                  variant="outlined"
+                  onChange={handleChange}
+                />
+                <FormControl>
+                  <FormLabel id="demo-radio-buttons-group-label">
+                    Pohlaví
+                  </FormLabel>
+                  <RadioGroup
+                    aria-labelledby="demo-radio-buttons-group-label"
+                    defaultValue=""
+                    name="gender"
+                    onChange={handleChange}
+                  >
+                    <FormControlLabel
+                      value="female"
+                      control={<Radio />}
+                      label="Žena"
+                    />
+                    <FormControlLabel
+                      value="male"
+                      control={<Radio />}
+                      label="Muž"
+                    />
+                  </RadioGroup>
+                </FormControl>
+                <FormControl>
+                  <FormLabel id="demo-radio-buttons-group-label">Cíl</FormLabel>
+                  <RadioGroup
+                    aria-labelledby="demo-radio-buttons-group-label"
+                    defaultValue=""
+                    name="target"
+                    onChange={handleChange}
+                  >
+                    <FormControlLabel
+                      value="weightloss"
+                      control={<Radio />}
+                      label="hubnutí"
+                    />
+                    <FormControlLabel
+                      value="maintenance"
+                      control={<Radio />}
+                      label="udržování"
+                    />
+                    <FormControlLabel
+                      value="gaining"
+                      control={<Radio />}
+                      label="nabírání"
+                    />
+                  </RadioGroup>
+                </FormControl>
+                <FormControl>
+                  <FormLabel id="demo-radio-buttons-group-label">
+                    Kolik hodin spánku máš
+                  </FormLabel>
+                  <RadioGroup
+                    aria-labelledby="demo-radio-buttons-group-label"
+                    defaultValue=""
+                    name="sleep"
+                    onChange={handleChange}
+                  >
+                    <FormControlLabel
+                      value="5"
+                      control={<Radio />}
+                      label="5 a méně"
+                    />
+                    <FormControlLabel
+                      value="6"
+                      control={<Radio />}
+                      label="6-7 hodin"
+                    />
+                    <FormControlLabel
+                      value="8"
+                      control={<Radio />}
+                      label="8 a více hodin"
+                    />
+                  </RadioGroup>
+                </FormControl>
+                <FormControl>
+                  <Stack spacing={1}>
+                    <InputLabel id="demo-simple-select-label">
+                      Aktivita
+                    </InputLabel>
 
-      {Object.values(values).length === 8 ? (
-        <Button variant="contained" onClick={handleCLick}>
-          Odeslat
-        </Button>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      name="activity"
+                      label="activity"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value="answer1">
+                        'Sedavé zaměstnání + žádný trénink'
+                      </MenuItem>
+                      <MenuItem value="answer2">
+                        {' '}
+                        'Sedavé zaměstnání + trénink vyšší intenzity
+                        (posilování, funkční trénink, kruhový trénink, běh,
+                        plavání apod.) 2x týdně, případně vícekrát týdně o nižší
+                        intenzitě (jóga, pilates)'
+                      </MenuItem>
+                      <MenuItem value="answer3">
+                        'Sedavé zaměstnání + trénink vyšší intenzity 3x- 4x
+                        týdně; nebo manuální těžké zaměstnání',
+                      </MenuItem>
+                      <MenuItem value="answer4">
+                        'Sedavé zaměstnání + trénink vyšší intenzity 6x týdně a
+                        více; nebo manuální těžké zaměstnání + trénink vyšší
+                        intenzity 2x týdně'
+                      </MenuItem>
+                      <MenuItem value="answer5">
+                        'Manuální těžké zaměstnání + trénink vyšší intenzity 3x
+                        – 4x týdně'
+                      </MenuItem>
+                      <MenuItem value="answer6">
+                        {' '}
+                        'Manuální těžké zaměstnání ě trénink vyšší intenzity 6x
+                        týdně a více (profesionální sportovci, trenéři a
+                        podobně)'
+                      </MenuItem>
+                    </Select>
+                  </Stack>
+                </FormControl>
+                <FormControl>
+                  <Stack spacing={1}>
+                    <InputLabel id="demo-simple-select-label">
+                      Jak často chceš cvičit
+                    </InputLabel>
+
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      name="intensity"
+                      label="intensity"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value="1">1</MenuItem>
+                      <MenuItem value="2">2</MenuItem>
+                      <MenuItem value="3">3</MenuItem>
+                      <MenuItem value="4">4</MenuItem>
+                      <MenuItem value="5">5</MenuItem>
+                      <MenuItem value="6">6</MenuItem>
+                      <MenuItem value="7">7</MenuItem>
+                    </Select>
+                  </Stack>
+                </FormControl>
+              </Stack>
+            </Paper>
+          </Box>
+
+          <Button
+            variant="contained"
+            disabled={!isFilled}
+            onClick={handleSubmit}
+          >
+            Odeslat
+          </Button>
+        </>
       ) : (
-        <Button variant="contained" disabled>
-          Odeslat
-        </Button>
+        <Box>
+          <Result values={[values]} />
+        </Box>
       )}
-
-      <Box>
-        <Result values={[values]} />
-      </Box>
     </>
   );
 };
