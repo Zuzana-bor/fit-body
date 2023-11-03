5a0af55e60761bc233326eef7dfe1fd387c3940c
Zuzana-bor <z.kasikova@gmail.com>
2023-11-03 08:45:03 +0100

    Úprava links v Mymenu a plans.

---
+++ src/pages/plans/FullBody.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FullBody = () => {
+  return <>FullBody</>;
+};
+
+export default FullBody;
---
+++ src/pages/plans/LowerBody.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LowerBody = () => {
+  return <>LowerBody</>;
+};
+
+export default LowerBody;
