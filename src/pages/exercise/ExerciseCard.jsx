d272c9a2e97a4f00626e54a179007f0c9a0b46c3
Zuzana-bor <z.kasikova@gmail.com>
2023-11-06 16:10:21 +0100

    uprava

--- src/pages/exercise/ExerciseCard.jsx
+++ src/pages/exercise/ExerciseCard.jsx
@@ -3,59 +3,27 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Box } from '@mui/material';
-import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body3,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
-
-const ExerciseCard = ({
-  item: { name, keys, description, media, category, musclesTargeted },
-}) => {
-  const [expend, setExpend] = React.useState(false);
-
+const ExerciseCard = ({ item: { name, description, media } }) => {
   return (
-    <Box>
-      <Item>
-        <Card sx={{ height: 300 }}>
-          {!expend ? (
-            <>
-              <CardMedia
-                component="img"
-                image={media}
-                title={name}
-                onClick={() => setExpend(!expend)}
-              />
-              <Typography gutterBottom variant="h8">
-                {name}
-              </Typography>
-            </>
-          ) : (
-            <CardContent
-              sx={{ overflowX: 'scroll' }}
-              onClick={() => setExpend(!expend)}
-            >
-              <Typography gutterBottom variant="h6">
-                {name}
-              </Typography>
-              {description}
-              <Typography
-                width="100%"
-                variant="body2"
-                color="text.secondary"
-                p={0.5}
-              ></Typography>
-            </CardContent>
-          )}
-        </Card>
-      </Item>
-    </Box>
+    <Card sx={{ height: '100%' }}>
+      <CardContent>
+        <CardMedia
+          sx={{ height: 250, objectFit: 'contain' }}
+          component="img"
+          image={media}
+          title={name}
+        />
+
+        <Typography gutterBottom variant="h6">
+          {name}
+        </Typography>
+
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 };
 
