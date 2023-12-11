3036a7798db96a498043315e6a4edde83e528c9f
Zuzana-bor <z.kasikova@gmail.com>
2023-12-11 13:18:47 +0100

    localstorage

--- src/store/AppContextProvider.tsx
+++ src/store/AppContextProvider.tsx
@@ -1,14 +1,17 @@
-import { ReactNode, useState } from 'react';
+import { ReactNode } from 'react';
 import { AppContext } from './AppContext ';
 import { FormAnswers, initialFormAnswers } from '../config';
+import { useLocalStorage } from '@uidotdev/usehooks';
 
 type Props = {
   children: ReactNode;
 };
 
 export const AppContextProvider = ({ children }: Props) => {
-  const [formAnswers, setFormAnswers] =
-    useState<FormAnswers>(initialFormAnswers);
+  const [formAnswers, setFormAnswers] = useLocalStorage<FormAnswers>(
+    'formAnswers',
+    initialFormAnswers,
+  );
 
   return (
     <AppContext.Provider value={{ formAnswers, setFormAnswers }}>
