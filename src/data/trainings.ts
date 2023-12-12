71302612c4770f5baf7c750e61f95f7bf1d859bd
Zuzana-bor <z.kasikova@gmail.com>
2023-12-12 12:24:56 +0100

    oprava exerciseId

--- src/data/trainings.ts
+++ src/data/trainings.ts
@@ -1,9 +1,6 @@
 export type TrainingDataPart = {
-  name?: string;
   number: string;
-  muscle?: string;
-  media?: string;
-  exerciseId?: string;
+  exerciseId: string;
 };
 
 export type TrainingData = TrainingDataPart[];
@@ -19,51 +16,51 @@ export const createTrainingPart = (
 
 const upperBody: TrainingData = [
   { exerciseId: 'jumping-jack', number: '1 minuta' },
-  { exerciseId: 'bicepsové-zdvihy-s-rotací', number: '15 / 3' },
-  { exerciseId: 'tricepsove-tlaky', number: '15 / 2' },
-  { exerciseId: 'arnoldovy-tlaky', number: '20 / 3' },
-  { exerciseId: 'pritahy-jednorucek-v-predklonu', number: '20 / 3' },
-  { exerciseId: 'kliky-damske', number: '15 / 2' },
-  { exerciseId: 'anglicak', number: '10 / 2' },
-  { exerciseId: 'zvedani-paží-v-planku', number: '12 / 2' },
-  { exerciseId: 'sedy-lehy', number: '30 / 3' },
-  { exerciseId: 'vykopavani-v-obracenem-vzporu', number: '20 / 3' },
+  { exerciseId: 'inner-bicep-curl', number: '15 / 3' },
+  { exerciseId: 'tricep-overhead-extensions', number: '15 / 2' },
+  { exerciseId: 'arnoldpress', number: '20 / 3' },
+  { exerciseId: 'single-arm-dumbbell-row', number: '20 / 3' },
+  { exerciseId: 'push-ups', number: '15 / 2' },
+  { exerciseId: 'burpee', number: '10 / 2' },
+  { exerciseId: 'plank-arm-lifts', number: '12 / 2' },
+  { exerciseId: 'sit-ups', number: '30 / 3' },
+  { exerciseId: 'reverse-Plank-Kicks', number: '20 / 3' },
 ];
 
 const fullBody = [
-  { exerciseId: 'švihadlo', number: '1 minuta' },
-  { exerciseId: 'dřep-s-jednoručkama', number: '30/3' },
-  { exerciseId: 'výpady-vzad/vpřed', number: '20/3' },
-  { exerciseId: 'unožování-ve-stoje-s-expanderem', number: '20/3' },
-  { exerciseId: 'kliky-damske', number: '15/2' },
-  { exerciseId: 'upažování-ve-stoje', number: '15/2' },
-  { exerciseId: 'bicepsove-zdvihy-s-rotací', number: '15/3' },
-  { exerciseId: 'tricepsové-tlaky', number: '15/2' },
-  { exerciseId: 'obrácený-anděl', number: '10/3' },
-  { exerciseId: 'zkracovačky-do-kříže-anděl', number: '30/3' },
-  { exerciseId: 'planky', number: '50 sekund/2' },
+  { exerciseId: 'jumping-rope', number: '1 minuta' },
+  { exerciseId: 'dumbbell-squat', number: '30/3' },
+  { exerciseId: 'pendulum-lunge', number: '20/3' },
+  { exerciseId: 'side-leg-lift', number: '20/3' },
+  { exerciseId: 'push-Ups', number: '15/2' },
+  { exerciseId: 'dumbbell-Standing-Lateral-Raise', number: '15/2' },
+  { exerciseId: 'inner-bicep-curl', number: '15/3' },
+  { exerciseId: 'tricep-overhead-extensions', number: '15/2' },
+  { exerciseId: 'reverse-snow-angle', number: '10/3' },
+  { exerciseId: 'cross-Crunch', number: '30/3' },
+  { exerciseId: 'plank', number: '50 sekund/2' },
 ];
 
 const lowerBody = [
   { exerciseId: 'jumping-jack', number: '1 minuta' },
-  { exerciseId: 'mrtvý-tah', number: '20 / 3' },
-  { exerciseId: 'sumo-dřep-s-jednoručkama', number: '20 / 3' },
-  { exerciseId: 'dřep-s-výskokem', number: '20 / 3' },
-  { exerciseId: 'výpady-do-strany', number: '20 / 3' },
-  { exerciseId: 'výpady-s-výskokem', number: '30 / 3' },
-  { exerciseId: 'angličáky', number: '10 / 2' },
-  { exerciseId: 'zvedání-pánve-v-leže', number: '15 / 2' },
+  { exerciseId: 'dumbbell-deadlifts', number: '20 / 3' },
+  { exerciseId: 'dumbbell-sumo-deadlift', number: '20 / 3' },
+  { exerciseId: 'jump-squat', number: '20 / 3' },
+  { exerciseId: 'side-Lunge', number: '20 / 3' },
+  { exerciseId: 'power-lunge', number: '30 / 3' },
+  { exerciseId: 'burpee', number: '10 / 2' },
+  { exerciseId: 'pelvic-tilt', number: '15 / 2' },
 ];
 
 const cardio = [
-  { exerciseId: 'švihadlo', number: '1 minuta' },
+  { exerciseId: 'jumping-rope', number: '1 minuta' },
   { exerciseId: 'jumping-jack', number: '1 minuta' },
-  { exerciseId: 'sedy-lehy', number: '30 / 3' },
-  { exerciseId: 'angličáky', number: '10 / 2' },
-  { exerciseId: 'dřep-s-výskokem', number: '20 / 3' },
-  { exerciseId: 'výpady-s-jednoručkama', number: '20 / 3' },
-  { exerciseId: 'výpady-s-výskokem', number: '30 / 3' },
-  { exerciseId: 'kliky-damske', number: '15/2' },
+  { exerciseId: 'sit-ups', number: '30 / 3' },
+  { exerciseId: 'burpee', number: '10 / 2' },
+  { exerciseId: 'jump-squat', number: '20 / 3' },
+  { exerciseId: 'side-lunge', number: '20 / 3' },
+  { exerciseId: 'power-lunge', number: '30 / 3' },
+  { exerciseId: 'push-ups', number: '15/2' },
 ];
 
 export type TrainingDataObject = {
