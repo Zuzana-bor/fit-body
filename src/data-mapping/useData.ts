56523b27b9a703d4a9b3c23920c8f1e85d640392
Zuzana-bor <z.kasikova@gmail.com>
2024-01-15 13:04:05 +0100

    fetchApiData

--- src/data-mapping/useData.ts
+++ src/data-mapping/useData.ts
@@ -4,52 +4,13 @@ import { Exercise } from '../data/exercises';
 import { getTrainings } from './trainings';
 import { TrainingsData } from '../data/trainings';
 import { TrainingPlan } from '../data/trainingsPlans';
+import { fetchApiData } from './utils';
 
 const useData = () => {
   const [loading, setLoading] = useState(false);
-
   const [exercises, setExercises] = useState<Exercise[]>();
-
-  const fetchExercises = async () => {
-    const exercisesResponse = await fetch(`${ApiUrls.Exercises}?delay=0`);
-
-    if (exercisesResponse.ok) {
-      const exercisesResult = await exercisesResponse.json();
-
-      setExercises(exercisesResult);
-    } else {
-      console.error('Response cannot be parsed', exercisesResponse);
-    }
-  };
   const [trainingsData, setTrainingsData] = useState<TrainingsData>();
-
-  const fetchTrainings = async () => {
-    const response = await fetch(`${ApiUrls.Trainings}?delay=0`);
-
-    if (response.ok) {
-      const result = await response.json();
-
-      setTrainingsData(result);
-    } else {
-      console.error('Response cannot be parsed', response);
-    }
-  };
-
   const [trainingPlans, setTrainingPlans] = useState<TrainingPlan[]>();
-
-  const fetchTrainingPlans = async () => {
-    const trainingPlansResponse = await fetch(
-      `${ApiUrls.TrainingPlans}?delay=0`,
-    );
-
-    if (trainingPlansResponse.ok) {
-      const trainingPlansResult = await trainingPlansResponse.json();
-
-      setTrainingPlans(trainingPlansResult);
-    } else {
-      console.error('Response cannot be parsed', trainingPlansResponse);
-    }
-  };
 
   const trainings =
     exercises && trainingsData
@@ -59,9 +20,9 @@ const useData = () => {
   const fetchData = async () => {
     setLoading(true);
     await Promise.all([
-      fetchExercises(),
-      fetchTrainings(),
-      fetchTrainingPlans(),
+      fetchApiData(ApiUrls.Exercises, setExercises),
+      fetchApiData(ApiUrls.Trainings, setTrainingsData),
+      fetchApiData(ApiUrls.TrainingPlans, setTrainingPlans),
     ]);
     setLoading(false);
   };
