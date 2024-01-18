ed75fef84de61eb6048c16a26b94d3c4d5d0d9be
Zuzana-bor <80903833+Zuzana-bor@users.noreply.github.com>
2024-01-18 18:20:20 +0100

    Nav bar respo (#32)
    * respons menu-hamburger
    
    * fixed closing hamburgermenu

--- src/layout/TopMenu.tsx
+++ src/layout/TopMenu.tsx
@@ -6,26 +6,80 @@ import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import { Pages, Urls } from '../config';
 import { Link as RouterLink } from 'react-router-dom';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+
+import MenuIcon from '@mui/icons-material/Menu';
+import { Drawer, IconButton, Link, List, ListItemButton } from '@mui/material';
 
 const TopMenu = () => {
+  const theme = useTheme();
+
+  const isMatch = useMediaQuery(theme.breakpoints.down('md'));
+
+  const [state, setState] = React.useState({ left: false });
+
+  const toggleDrawer =
+    (anchor: 'left', open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+
   return (
     <AppBar position="static">
       <Container>
         <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {(Object.entries(Pages) as [keyof typeof Urls, string][]).map(
-              ([url, name]) => (
-                <Button
-                  component={RouterLink}
-                  to={Urls[url]}
-                  key={url}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
+          {isMatch === false ? (
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {(Object.entries(Pages) as [keyof typeof Urls, string][]).map(
+                ([url, name]) => (
+                  <Button
+                    component={RouterLink}
+                    to={Urls[url]}
+                    key={url}
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                  >
+                    {name}
+                  </Button>
+                ),
+              )}
+            </Box>
+          ) : (
+            <div>
+              <React.Fragment>
+                <IconButton onClick={toggleDrawer('left', true)}>
+                  <MenuIcon />
+                </IconButton>
+                <Drawer
+                  anchor="left"
+                  open={state['left']}
+                  onClose={toggleDrawer('left', false)}
                 >
-                  {name}
-                </Button>
-              ),
-            )}
-          </Box>
+                  <List>
+                    {(
+                      Object.entries(Pages) as [keyof typeof Urls, string][]
+                    ).map(([url, name]) => (
+                      <ListItemButton
+                        component={RouterLink}
+                        to={Urls[url]}
+                        key={url}
+                      >
+                        {name}
+                      </ListItemButton>
+                    ))}
+                  </List>
+                </Drawer>
+              </React.Fragment>
+            </div>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
