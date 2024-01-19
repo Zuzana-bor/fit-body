46b3a3e65ad27d5a52e984da3ab91e848b6a3c93
Zuzana-bor <80903833+Zuzana-bor@users.noreply.github.com>
2024-01-19 08:23:38 +0100

    Plans resp (#37)
    * menu respo
    
    * plány resp
    
    * smazány unused

--- src/pages/plans/TrainingTable.tsx
+++ src/pages/plans/TrainingTable.tsx
@@ -13,50 +13,90 @@ import { Training } from '../../data-mapping/trainings';
 import PageLoader from '../../layout/PageLoader';
 import { AppContext } from '../../store/AppContext ';
 
+import {
+  List,
+  ListItemButton,
+  ListItemText,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
+
 type TrainingTableProps = {
   training: Training;
 };
 
 const TrainingTable: FC<TrainingTableProps> = ({ training }) => {
   const { loading } = React.useContext(AppContext);
+  const theme = useTheme();
+  const isMatch = useMediaQuery(theme.breakpoints.down('md'));
   return (
-    <TableContainer component={Paper}>
-      {loading && <PageLoader />}
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Cvik</TableCell>
-            <TableCell align="right">Počet opakování/ sérií</TableCell>
-            <TableCell align="right">Svalová partie</TableCell>
-            <TableCell align="right">Jak na to</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {training.map(
-            ({ number, exercise: { mainMuscleGroupe, media, name, id } }) => (
-              <TableRow
-                key={name}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  <Link to={`${Urls.Exercise}#${id}`}>{name}</Link>
-                </TableCell>
-                <TableCell align="right">{number}</TableCell>
-                <TableCell align="right">{mainMuscleGroupe}</TableCell>
-                <TableCell align="right">
+    <>
+      {!isMatch ? (
+        <TableContainer component={Paper}>
+          {loading && <PageLoader />}
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Cvik</TableCell>
+                <TableCell align="right">Počet opakování/ sérií</TableCell>
+                <TableCell align="right">Svalová partie</TableCell>
+                <TableCell align="right">Jak na to</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {training.map(
+                ({
+                  number,
+                  exercise: { mainMuscleGroupe, media, name, id },
+                }) => (
+                  <TableRow
+                    key={name}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      <Link to={`${Urls.Exercise}#${id}`}>{name}</Link>
+                    </TableCell>
+                    <TableCell align="right">{number}</TableCell>
+                    <TableCell align="right">{mainMuscleGroupe}</TableCell>
+                    <TableCell align="right">
+                      <CardMedia
+                        sx={{ height: 100, objectFit: 'contain' }}
+                        component="img"
+                        image={media}
+                        title={name}
+                      />
+                    </TableCell>
+                  </TableRow>
+                ),
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <>
+          <List>
+            {training.map(
+              ({ number, exercise: { mainMuscleGroupe, media, name, id } }) => (
+                <ListItemButton key={name}>
                   <CardMedia
-                    sx={{ height: 100, objectFit: 'contain' }}
+                    sx={{ height: 100, width: 75, objectFit: 'contain' }}
                     component="img"
                     image={media}
                     title={name}
                   />
-                </TableCell>
-              </TableRow>
-            ),
-          )}
-        </TableBody>
-      </Table>
-    </TableContainer>
+                  <ListItemText
+                    primary={<Link to={`${Urls.Exercise}#${id}`}>{name}</Link>}
+                    secondary={`počet opakování/sérií: ${number} 
+                    svalová partie: ${mainMuscleGroupe}
+                    `}
+                  />
+                </ListItemButton>
+              ),
+            )}
+          </List>
+        </>
+      )}{' '}
+    </>
   );
 };
 export default TrainingTable;
