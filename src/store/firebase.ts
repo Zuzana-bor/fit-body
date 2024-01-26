de6c0d3b058cfab4a85a3cad3813783c16d835bb
Zuzana-bor <z.kasikova@gmail.com>
2024-01-26 13:37:53 +0100

    oprava unused

--- src/store/firebase.ts
+++ src/store/firebase.ts
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyAxt-W8zN4hcZCFEfR6HOPx07j1ngQDSnY',
   authDomain: 'fit-body-a6d91.firebaseapp.com',
@@ -14,6 +10,5 @@ const firebaseConfig = {
   appId: '1:1005547013108:web:5f630cfe23faa799832fa1',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
