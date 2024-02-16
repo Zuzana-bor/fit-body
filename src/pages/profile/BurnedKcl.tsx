2aabda47f9dd55e901b748e33bb40912ab5621ea
Zuzana-bor <80903833+Zuzana-bor@users.noreply.github.com>
2024-02-16 08:42:11 +0100

    deleted unused (#48)

--- src/pages/profile/BurnedKcl.tsx
+++ src/pages/profile/BurnedKcl.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+import { Card, CardContent, Typography } from '@mui/material';
 import React from 'react';
 
 import { AppContext } from '../../store/AppContext ';
